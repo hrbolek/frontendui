fd4abb5feb04be61c712fe98f7cf8628301ef2f7
Maya0552 <magdalena.cetlova@unob.cz>
2026-07-20 00:22:20 +0200

    publikace vol.2

--- packages/semester_majda/src/SemestrGQLModel/Components/Table.jsx
+++ packages/semester_majda/src/SemestrGQLModel/Components/Table.jsx
@@ -1,7 +1,54 @@
-import { Table as BaseTable } from "../../../../_template/src/Base/Components/Table" 
+import { useMemo } from "react";
+
+import {
+    Table as BaseTable,
+    buildTableDef,
+} from "../../../../_template/src/Base/Components/Table";
+
+import {
+    Link as SemesterLink,
+    StudyPlanLink,
+} from "./Link";
+
+const DirectIdCell = ({ row, name }) => {
+    const ItemLink =
+        row?.__typename === "StudyPlanGQLModel"
+            ? StudyPlanLink
+            : SemesterLink;
+
+    return (
+        <td key={name}>
+            <ItemLink item={row}>
+                {row?.id || "Data error"}
+            </ItemLink>
+        </td>
+    );
+};
 
 export const Table = ({ data }) => {
+    const table_def = useMemo(() => {
+        if (!data || data.length === 0) {
+            return {};
+        }
+
+        const definition = buildTableDef(data);
+
+        definition.id = {
+            label: definition.id?.label || "id",
+            component: DirectIdCell,
+        };
+
+        return definition;
+    }, [data]);
+
+    if (!data || data.length === 0) {
+        return null;
+    }
+
     return (
-        <BaseTable data={data} />
-    )
-}
+        <BaseTable
+            data={data}
+            table_def={table_def}
+        />
+    );
+};
